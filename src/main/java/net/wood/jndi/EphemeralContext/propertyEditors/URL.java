c9357dfb8d6ab020bd04ccb156142a61f8e39d51
Mark H. Wood <mhwood@Ameritech.Net>
2012-02-05 17:32:31 -0500

    Add a URL type.  (Not yet implemented)

---
+++ src/main/java/net/wood/jndi/EphemeralContext/propertyEditors/URL.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2012 Mark H. Wood
+ */
+
+package net.wood.jndi.EphemeralContext.propertyEditors;
+
+import org.xml.sax.Attributes;
+
+/**
+ *
+ * @author mhwood
+ */
+public class URL
+implements PropertyEditor
+{
+    public Object interpret(String uri, String localName, String qName,
+            Attributes attributes)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
