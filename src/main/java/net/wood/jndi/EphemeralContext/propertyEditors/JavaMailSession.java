8819fae3821c37c1e6d1be03199aeebbd9cb077f
Mark H. Wood <mhwood@Ameritech.Net>
2012-06-06 22:21:12 -0400

    Add authenticated sending

--- src/main/java/net/wood/jndi/EphemeralContext/propertyEditors/JavaMailSession.java
+++ src/main/java/net/wood/jndi/EphemeralContext/propertyEditors/JavaMailSession.java
@@ -5,6 +5,8 @@
 package net.wood.jndi.EphemeralContext.propertyEditors;
 
 import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import org.xml.sax.Attributes;
 
@@ -16,8 +18,12 @@ import org.xml.sax.Attributes;
  * @author mhwood
  */
 public class JavaMailSession
-implements PropertyEditor
+        extends Authenticator
+        implements PropertyEditor
 {
+    private String username = null;
+    private String password = "";
+
     public Object interpret(String uri, String localName, String qName,
             Attributes attributes)
     {
@@ -25,22 +31,42 @@ implements PropertyEditor
         int nAttribute;
         if ((nAttribute = attributes.getIndex("storeProtocol")) >= 0)
             sessionProps.setProperty("mail.store.protocol", attributes.getValue(nAttribute));
+
         if ((nAttribute = attributes.getIndex("transferProtocol")) >= 0)
             sessionProps.setProperty("mail.transfer.protocol", attributes.getValue(nAttribute));
+
         if ((nAttribute = attributes.getIndex("host")) >= 0)
             sessionProps.setProperty("mail.host", attributes.getValue(nAttribute));
+
         if ((nAttribute = attributes.getIndex("user")) >= 0)
-            sessionProps.setProperty("mail.user", attributes.getValue(nAttribute));
-        // TODO mail.password
+        {
+            username = attributes.getValue(nAttribute);
+            sessionProps.setProperty("mail.user", username);
+        }
+
+        if ((nAttribute = attributes.getIndex("password")) >= 0)
+            password = attributes.getValue(nAttribute);
+
         if ((nAttribute = attributes.getIndex("from")) >= 0)
             sessionProps.setProperty("mail.from", attributes.getValue(nAttribute));
+
         if (attributes.getIndex("debug") >= 0)
             sessionProps.setProperty("mail.debug", "true");
         // TODO mail.PROTOCOL.host
         // TODO mail.PROTOCOL.user
         // TODO mail.PROTOCOL.port
-        Session session = Session.getInstance(sessionProps); // TODO Authenticator
+
+        Session session;
+        if (null == username)
+            session = Session.getInstance(sessionProps);
+        else
+            session = Session.getInstance(sessionProps, this);
         return session;
     }
-    
+
+    @Override
+    protected PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(username, password);
+    }
 }
