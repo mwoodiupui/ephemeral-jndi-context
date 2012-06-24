e57405096421be7e4e2097e802066d833e606140
Mark H. Wood <mhwood@Ameritech.Net>
2012-06-24 12:57:17 -0400

    JUnit 4 style, separate mail and URL into different methods.

--- src/test/java/net/wood/jndi/EphemeralContext/EphemeralContextTest.java
+++ src/test/java/net/wood/jndi/EphemeralContext/EphemeralContextTest.java
@@ -5,42 +5,25 @@
 
 package net.wood.jndi.EphemeralContext;
 
+import java.net.URL;
 import javax.mail.Session;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 /**
  * Unit test for EphemeralContext.
  */
 public class EphemeralContextTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public EphemeralContextTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( EphemeralContextTest.class );
-    }
-
     /**
      * Test basic lookup, path traversal, returned object types.
      */
-    public void testEphemeralContext()
+    @Test
+    public void testPostgreSQLDataSource()
             throws NamingException
     {
         // initial-context factory, URL supplied externally
@@ -52,21 +35,49 @@ public class EphemeralContextTest
         Context dc = (Context) ec.lookup("jdbc");
 
         Object o = dc.lookup("test");
-        assertTrue(o instanceof DataSource);
+        assertTrue("Expecting a DataSource", o instanceof DataSource);
 
         Object o2 = ic.lookup("java:comp/env/jdbc/test");
-        assertTrue(o2 instanceof DataSource);
-        
+        assertTrue("Expecting a DataSource", o2 instanceof DataSource);
+
         Object o3 = ec.lookup("jdbc/test");
-        assertTrue(o3 instanceof DataSource);
+        assertTrue("Expecting a DataSource", o3 instanceof DataSource);
 
         // Check incorrect but apparently common form
         Context c1 = (Context) ic.lookup("java:/comp/env");
         Object ds1 = c1.lookup("jdbc/test");
-        assertTrue(ds1 instanceof DataSource);
-        
+        assertTrue("Expecting a DataSource", ds1 instanceof DataSource);
+
+        // TODO test creation of new objects in the directory
+
+        // TODO test deletion of directory objects
+
+        // TODO test for nonexistent object
+    }
+
+    @Test
+    public void testMail() throws NamingException
+    {
+        // initial-context factory, URL supplied externally
+        javax.naming.Context ic = new InitialContext(null);
+
         // java:comp/env/mail/Session
         Object om = ic.lookup("java:comp/env/mail/Session");
-        assertTrue(om instanceof Session);
+        assertTrue("Expecting a mail Session", om instanceof Session);
+    }
+
+    @Test
+    public void testURL() throws NamingException
+    {
+        // initial-context factory, URL supplied externally
+        javax.naming.Context ic = new InitialContext(null);
+
+        // java:comp/env/url/Example
+        Object ou = ic.lookup("java:comp/env/url/Example");
+        assertTrue("Expecting a URL", ou instanceof URL);
+        System.out.println("java:comp/env/url/Example = " + (URL)ou);
+
+        Object ou2 = ic.lookup("java:comp/env/url/BadExample");
+        assertNull("Invalid URL should return null object", ou2);
     }
 }
