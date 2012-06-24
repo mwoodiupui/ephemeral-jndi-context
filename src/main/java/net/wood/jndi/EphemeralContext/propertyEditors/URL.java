344731e0d5e4442290c2f1370a9b99f1f9305c21
Mark H. Wood <mhwood@Ameritech.Net>
2012-06-24 12:59:13 -0400

    Flesh out URL provider

--- src/main/java/net/wood/jndi/EphemeralContext/propertyEditors/URL.java
+++ src/main/java/net/wood/jndi/EphemeralContext/propertyEditors/URL.java
@@ -4,18 +4,42 @@
 
 package net.wood.jndi.EphemeralContext.propertyEditors;
 
+import java.net.MalformedURLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
 
 /**
- *
+ * Create a java.net.URL entry from the specified {@code value} attribute.
+ * <br />
+ * {@code <object class='net.wood.jndi.EphemeralContext.propertyEditors.URL'
+ *          value='http://www.example.com/foo/bar'/>}
  * @author mhwood
  */
 public class URL
 implements PropertyEditor
 {
+    private static final Logger log = LoggerFactory.getLogger(URL.class);
+
     public Object interpret(String uri, String localName, String qName,
             Attributes attributes)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String property = attributes.getValue("value");
+        java.net.URL result = null;
+
+        if (null == property)
+            log.warn("URL with no value attribute");
+        else
+        {
+            try {
+                result = new java.net.URL(property);
+            } catch (MalformedURLException ex) {
+                log.warn("Uninterpretable URL '{}':  {}",
+                        property, ex.getMessage());
+            }
+        }
+
+        log.debug("returning {}", result);
+        return result;
     }
 }
